517c1aad09f890a4980e32e06f032aacdc0204f1
Justin <zhou78620051@126.com>
2018-02-27 03:23:06 +0800

    测试aop的例子, 切面的 id:method 正则表达式

---
+++ examples/aop/app.js
@@ -0,0 +1,33 @@
+/**
+ * Created by examples.
+ * File: app.js
+ * User: justin
+ * Date: 26/2/2018
+ * Time: 00:21
+ */
+
+'use strict';
+
+
+let Bearcat = require('bearcat');
+let configPaths = [require.resolve('./context.json')];
+
+Bearcat.createApp(configPaths, {});
+Bearcat.start(function () {
+    console.log('===Bearcat IoC container started');
+    let bus = Bearcat.getBean('car');
+    bus.runBefore(function (err, data) {
+        console.log('===data %s err', data, err);
+    });
+    // car.runBeforeError(function (err, data){
+    //     console.log('===data %s err', data, err);
+    // });
+
+    let wheel = Bearcat.getBean('wheel');
+    wheel.runBefore(function (err, data) {
+        console.log('---data %s err', data, err);
+    });
+    // wheel.runBeforeError(function (err, data){
+    //     console.log('---data %s err', data, err);
+    // });
+});
--- examples/aop/wheel.js
+++ examples/aop/wheel.js
@@ -7,3 +7,18 @@ module.exports = Wheel;
 Wheel.prototype.doWheel = function() {
 
 }
+
+Wheel.prototype.runBeforeSync = function() {
+    console.log('   Wheel runBeforeSync wheel...');
+    return 'runBeforeSync';
+}
+
+Wheel.prototype.runBefore = function(cb) {
+    console.log('   Wheel runBefore wheel...');
+    cb(null, 'wheel');
+}
+
+Wheel.prototype.runBeforeError = function(cb) {
+    console.log('   Wheel runBeforeError wheel...');
+    cb(null, 'wheel');
+}
